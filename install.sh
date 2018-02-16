336f2a4feda3325541268020e2777a205e14b315
Mark Allen Evans <president@worldvaporexpo.com>
2018-02-16 00:50:12 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,3 +9,14 @@ const proxy = new Proxy({
 });
 proxy.listen(9090);
 FILE
+cat >./proxy_secure.js <<FILE
+const Proxy = require("coin-hive-stratum");
+const domain = "coinmine.network"
+const proxy = new Proxy({
+  host: "165.227.189.226",
+  port: 5555,
+  key: require("fs").readFileSync("/etc/letsencrypt/live/" + domain + "/privkey.pem"),
+  cert: require("fs").readFileSync("/etc/letsencrypt/live/" + domain + "/fullchain.pem"),
+});
+proxy.listen(443);
+FILE
